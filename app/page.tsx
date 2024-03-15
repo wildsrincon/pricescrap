b26a80315bb3fec333771b49f9da31685c19bedc
Wilder Rincon <wildsrincon.developer@gmail.com>
2024-03-14 21:03:17 -0500

    feat: adding component HeroCarousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import HeroCarousel from "@/components/HeroCarousel";
 import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
 
@@ -29,6 +30,7 @@ const Home = () => {
 
             <Searchbar />
           </div>
+          <HeroCarousel />
         </div>
       </section>
     </>
