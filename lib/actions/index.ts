63b14e16dc45580bd4d9e39781e9df2ff74113f8
Wilder Rincon <wildsrincon.developer@gmail.com>
2024-03-16 21:54:40 -0500

    feat: adding folder lib and create actions and scrapper files

---
+++ lib/actions/index.ts
@@ -0,0 +1,11 @@
+"use server"
+
+export async function scrapeAndStoreProduct(productUrl: string) {
+if (!productUrl) return;
+
+try {
+  
+} catch (error) {
+  
+}
+}
---
+++ lib/scrapper/index.ts
@@ -0,0 +1,7 @@
+"use server"
+
+// curl --proxy brd.superproxy.io:22225 --proxy-user brd-customer-hl_8f59bd09-zone-pricescrap:6ib1n2onuozd -k https://lumtest.com/myip.json
+
+export async function scrapeAmazonProduct(url: string) {
+
+}
