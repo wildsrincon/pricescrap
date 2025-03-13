6457cb3801b49ff878f84c019620c5d7cb9a88f7
Wilder Rincon <wildsrincon.developer@gmail.com>
2025-03-12 20:36:19 -0500

    fix: fix typo import scrapper

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/utils";
 import { connectToDB } from "@/lib/mongoose";
 import Product from "@/lib/models/product.model";
-import { scrapeAmazonProduct } from "@/lib/scraper";
+import { scrapeAmazonProduct } from "@/lib/scrapper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
 export const maxDuration = 300; // This function can run for a maximum of 300 seconds
